ea495ae20db191e24d7619d356f5dad2f03484e1
mayipeng <356287372@qq.com>
2021-01-20 20:36:04 +0800

    。

--- pmms2_provider/src/main/java/com/diantong4/pmms2_provider/DubboProviderApplication.java
+++ pmms2_provider/src/main/java/com/diantong4/pmms2_provider/DubboProviderApplication.java
@@ -13,5 +13,6 @@ public class DubboProviderApplication {
     public static void main(String[] args) {
         System.out.println("生产者已经启动");
         SpringApplication.run(DubboProviderApplication.class, args);
+
     }
 }
